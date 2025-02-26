9345df3551889c343f7fdc4865b6719fc7839bea
Dima Monid <1234567waka.waka@gmail.com>
2025-02-26 17:38:49 +0300

    aa

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SpringAnimation
 
 final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var springAnimationLabel: SpringLabel!{
+        didSet {
+            springAnimationLabel.text = animation.description
+        }
     }
+    
+    private var animation = Animation.randomAnimation
 
-
+    @IBAction func runButtonPressed(_ sender: UIButton) {
+        springAnimationLabel.animation = "fadeOut"
+        springAnimationLabel.animate()
+        
+        springAnimationLabel.animateNext { [unowned self] in
+            springAnimationLabel.text = animation.description
+            springAnimationLabel.animation = "fadeIn"
+            springAnimationLabel.animate()
+            
+            springAnimationView.animation = animation.name
+            springAnimationView.curve = animation.curve
+            springAnimationView.force = animation.force
+            springAnimationView.duration = animation.duration
+            springAnimationView.delay = animation.delay
+            springAnimationView.animate()
+            
+            animation = Animation.randomAnimation
+            sender.setTitle("Run \(animation.name)", for: .normal)
+        }
+        
+    }
+    
 }
 
