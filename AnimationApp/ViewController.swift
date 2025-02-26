a1792930a3d8a3a6e8076a4728d0fecda2f9b5b7
Dima Monid <1234567waka.waka@gmail.com>
2025-02-26 16:51:58 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Дима Монид on 26.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
