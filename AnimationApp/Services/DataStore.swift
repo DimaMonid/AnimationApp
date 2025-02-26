24cbb91260a7c78ea6d2e8d95af42393f1d29334
Dima Monid <1234567waka.waka@gmail.com>
2025-02-26 17:12:15 +0300

    mm

---
+++ AnimationApp/Services/DataStore.swift
@@ -0,0 +1,18 @@
+//
+//  DataStore.swift
+//  AnimationApp
+//
+//  Created by Дима Монид on 26.02.25.
+//
+
+import SpringAnimation
+
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let animations = AnimationPreset.allCases
+    let curves = AnimationCurve.allCases
+    
+    private init() {}
+}
